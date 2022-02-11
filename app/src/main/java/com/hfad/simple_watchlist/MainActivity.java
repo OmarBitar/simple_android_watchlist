3a854faae5f020a59b47ec67f6c278103df9a65e
Omar Bitar <o.bitar97@gmail.com>
2022-02-11 13:57:15 -0700

    switch fragments based on buttons clicked

--- app/src/main/java/com/hfad/simple_watchlist/MainActivity.java
+++ app/src/main/java/com/hfad/simple_watchlist/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		//more info on fragment transactions go to HEADFIRST PG 420
+		Fragment movieFrag = new moviesFragment();
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.replace(R.id.mainFragment,movieFrag).commit();
+	}
+
+	public void onClickSeries(View view) {
+		Fragment seriesFrag = new seriesFragment();
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.replace(R.id.mainFragment,seriesFrag).commit();
+	}
+
+	public void onClickMovies(View view) {
 		Fragment movieFrag = new moviesFragment();
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 		ft.replace(R.id.mainFragment,movieFrag).commit();
