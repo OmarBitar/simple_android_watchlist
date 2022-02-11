964bcbbf2257b7cfceb90f7fe8de8872f8e802c2
Omar Bitar <o.bitar97@gmail.com>
2022-02-11 15:46:38 -0700

    implemented simple string[] recycleView

--- app/src/main/java/com/hfad/simple_watchlist/moviesFragment.java
+++ app/src/main/java/com/hfad/simple_watchlist/moviesFragment.java
@@ -3,6 +3,8 @@ package com.hfad.simple_watchlist;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +15,15 @@ public class moviesFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_movies, container, false);
+
+		String movieData[] = {"batman","superman","inception","godfather","goodfellas"};
+		RecyclerView rc = (RecyclerView) view.findViewById(R.id.recycleView);
+		rc.setLayoutManager(new LinearLayoutManager(view.getContext()));
+		CustomAdapter adapter = new CustomAdapter(movieData);
+		rc.setAdapter(adapter);
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_movies, container, false);
+		return view;
 	}
 }
